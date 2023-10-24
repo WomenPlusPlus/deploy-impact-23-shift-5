d379c41426714b33b0be8ab55ea4f3cea25a74ed
iamlydial <lydia.lavecchia@gmail.com>
2023-10-24 17:22:19 +0200

    cors headers added inthe settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Define CORS settings
+CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin for development (configure for production)
+CORS_ALLOWED_ORIGIN = [
+    "host://localhost:5173"
+]
+CORS_ALLOW_CREDENTIALS = True  # Allow cookies and credentials to be sent with requests (if needed)
