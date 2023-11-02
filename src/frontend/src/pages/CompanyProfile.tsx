452465867ab24fd366a76fc365ed71b2198217df
iamlydial <lydia.lavecchia@gmail.com>
2023-11-02 12:53:21 +0100

    added company profile, switch on the company sidebar to newly created cerate job view

--- src/frontend/src/pages/CompanyProfile.tsx
+++ src/frontend/src/pages/CompanyProfile.tsx
@@ -1,23 +1,40 @@
+import { useState } from 'react'
 import CompanySidebar from '@/scenes/companysidebar'
 import CandidateNavbar from '@/scenes/candidatenavbar'
 import CompanyMainSection from '@/scenes/companymainsection'
+import CompanyMainSectionJobCreation from '@/scenes/companymainsectionjobcreation'
 
 type Props = {}
 
 const CompanyProfile = (props: Props) => {
+  const [activeSection, setActiveSection] = useState("CompanyMainSection");
+
+  const handleNewJobClick = () => {
+    setActiveSection("CompanyMainSectionJobCreation");
+  };
+
+  const handleProfileClick = () => {
+    setActiveSection("CompanyMainSection");
+  };
+
   return (
     <div className="flex flex-row">
-        <div className='flex flex-col min-w-fit'>
-            <CompanySidebar/>
-        </div>
-        <div>
-        <div className="flex flex-wrap">
-            <CandidateNavbar/>  
-            <CompanyMainSection />
-        </div>
+      <div className='flex flex-col min-w-fit'>
+        <CompanySidebar onNewJobClick={handleNewJobClick} onProfileClick={handleProfileClick}/>
+      </div>
+      <div className='flex flex-col w-full'>
+        <CandidateNavbar />
+        {activeSection === "CompanyMainSection" ? (
+          <CompanyMainSection />
+        ) : activeSection === 'CompanyMainSectionJobCreation' ? (
+          <CompanyMainSectionJobCreation />
+        ) : null}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default CompanyProfile
+
+
+
