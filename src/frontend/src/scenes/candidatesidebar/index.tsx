bedb5a05d868ebaf666920461188263e63a58600
Jeyl <gogersophie@gmail.com>
2023-11-02 20:27:34 +0100

    Icon path

--- src/frontend/src/scenes/candidatesidebar/index.tsx
+++ src/frontend/src/scenes/candidatesidebar/index.tsx
@@ -30,7 +30,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
         </div>
         <div className="flex flex-row justify-center">
           <div className="flex flex-col pr-4">
-            <Avatar src="./src/frontend/src/assets/candidates/EmilyPicture.png" alt="Picture" className="w-[80px] border-purple-70 border-2 rounded-full flex-shrink-01" />
+            <Avatar src="/src/assets/candidates/EmilyPicture.png" alt="Picture" className="w-[80px] border-purple-70 border-2 rounded-full flex-shrink-01" />
           </div>
           <div className="flex flex-col">
             <Typography variant="h6">Emily</Typography>
@@ -42,7 +42,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
       </div>
       <div className="m-2 flex-wrap items-center gap-4 pt-6">
         <button className="text-purple-70 font-roboto font-bold text-{17px} leading-5.5 w-5/6 rounded-full white p-2 border-2 mb-5 px-5 py-2.5 text-center inline-flex items-center mr-2">
-          <img className="w-5.5 h-5.5 ml-8 mr-1"src="./src/frontend/src/assets/candidates/mail_outline.png"></img>
+          <img className="w-5.5 h-5.5 ml-8 mr-1"src="/src/assets/candidates/mail_outline.png"></img>
           Inbox
         </button>
         <button 
@@ -53,7 +53,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           <hr className="my-2 border-blue-gray-50" />
           <ListItem className="p-2.5 mb-2 mt-2" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/icon-dashboard.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/icon-dashboard.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Overview
@@ -62,7 +62,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           <hr className="my-2 #CAC5CD" />
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/Search.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/Search.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto  text-purple-70">
                 Search
@@ -70,7 +70,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           </ListItem> 
           <ListItem className="mb-6 p-2.5 " selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/favorite.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/favorite.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Favorites
@@ -78,7 +78,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/Games.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/Games.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography 
                 onClick={() => switchSection('mymatches')}
@@ -88,7 +88,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/Grading.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/Grading.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Initiatives
@@ -96,7 +96,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/Description.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/Description.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 My Application
@@ -104,7 +104,7 @@ const CandidateSidebar: React.FC<CandidateSidebarProps> = ({ switchSection }) =>
           </ListItem> 
           <ListItem className="p-2.5 " selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/frontend/src/assets/candidates/Vector.png' className="h-6 w-6 mr-1"/>
+                <img src='/src/assets/candidates/Vector.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Help
--- src/frontend/src/scenes/companysidebar/index.tsx
+++ src/frontend/src/scenes/companysidebar/index.tsx
@@ -30,7 +30,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
         </div>
         <div className="flex flex-row justify-center">
           <div className="flex flex-col pr-4">
-            <Avatar src="./src/assets/SwissComLogo.png" alt="Picture" className="w-[80px] border-purple-70 border-2 rounded-full flex-shrink-01" />
+            <Avatar src="/src//assets/SwissComLogo.png" alt="Picture" className="w-[80px] border-purple-70 border-2 rounded-full flex-shrink-01" />
           </div>
           <div className="flex flex-col">
             <Typography variant="h6">Swisscom</Typography>
@@ -42,7 +42,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
       </div>
       <div className="flex flex-col items-center gap-4 pt-6 ">
         <button className="flex flex-row text-purple-70 font-roboto font-bold text-{17px} leading-5.5 w-5/6 rounded-full white p-2 border-2 px-5 ">
-          <img className="w-5.5 h-5.5 ml-8 mr-1"src="./src/assets/candidates/mail_outline.png"></img>
+          <img className="w-5.5 h-5.5 ml-8 mr-1"src="/src//assets/candidates/mail_outline.png"></img>
           Inbox
         </button>
         <button onClick={props.onProfileClick} className=" flex flex-row w-5/6 rounded-full bg-purple-50 p-2 font-bold text-{17px} justify-center font-roboto leading-5.5 text-white">Profile</button>
@@ -54,7 +54,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           <hr className="my-2 border-blue-gray-50" />
           <ListItem className="p-2.5 mb-2 mt-2" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/icon-dashboard.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/icon-dashboard.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Overview
@@ -63,7 +63,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           <hr className="my-2 #CAC5CD" />
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/Search.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/Search.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto  text-purple-70">
                 Search
@@ -71,7 +71,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           </ListItem> 
           <ListItem className="mb-6 p-2.5 " selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/favorite.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/favorite.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Favorites
@@ -79,7 +79,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/Games.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/Games.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 My Matches
@@ -87,7 +87,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/Grading.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/Grading.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Initiatives
@@ -95,7 +95,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           </ListItem> 
           <ListItem className="p-2.5 mb-6" selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/Description.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/Description.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 My Application
@@ -103,7 +103,7 @@ const CompanySidebar = (props: CompanySidebarProps) => {
           </ListItem> 
           <ListItem className="p-2.5 " selected={open === 0}>
               <ListItemPrefix>
-                <img src='./src/assets/candidates/Vector.png' className="h-6 w-6 mr-1"/>
+                <img src='/src//assets/candidates/Vector.png' className="h-6 w-6 mr-1"/>
               </ListItemPrefix>
               <Typography className="mr-auto font-bold text-sm/[17px] font-roboto text-purple-70">
                 Help
